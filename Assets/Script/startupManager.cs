59628116ca782b9d46a4cf2139510ddd537dd02e
Satsuna <jvbade@gmail.com>
2025-04-14 19:42:26 +0800

    added autologin

--- Assets/Script/startupManager.cs
+++ Assets/Script/startupManager.cs
@@ -6,6 +6,18 @@ public class startupManager : MonoBehaviour
 {
     public Toggle toggle;
     public Button button;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("HasLaunchedBefore")) {
+            SceneManager.LoadScene("Sign In");
+        }
+
+        else {
+            PlayerPrefs.SetInt("HasLaunchedBefore", 1);
+            PlayerPrefs.Save();
+        }
+    }
     public void TCToggle()
     {
         if(toggle.isOn == true) {
@@ -19,4 +31,6 @@ public class startupManager : MonoBehaviour
     public void changeScene() {
         SceneManager.LoadScene("Sign In");
     }
+
+
 }
